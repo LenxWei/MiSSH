cf1ea3a0ef23d94f83625123e2535621133f688b
Lenx Wei <lenx.wei@gmail.com>
2013-04-03 02:56:25 -0700

    workaround for mac osx term, 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "missh",
-    version = "0.1.1rc7",
+    version = "0.1.1",
 #    packages = find_packages(), #["mipass","missh-nox"],
     py_modules = ["mipass"],
     scripts = ['missh'],
