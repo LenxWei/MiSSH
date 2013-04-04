d06a82e5599b3a66d11b5f8f4984851648d261b7
Lenx Wei <lenx.wei@gmail.com>
2013-04-03 21:33:09 -0700

    0.1.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "missh",
-    version = "0.1.2",
+    version = "0.1.3",
 #    packages = find_packages(), #["mipass","missh-nox"],
     py_modules = ["mipass"],
     scripts = ['missh'],
